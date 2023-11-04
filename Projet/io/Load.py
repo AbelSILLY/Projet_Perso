b744788a005f19e8769244df1bc044f89a0b27bb
Abel SILLY <abel.silly@etu.umontpellier.fr>
2023-11-04 12:15:34 +0100

    récupération de données

--- Projet/io/Load.py
+++ Projet/io/Load.py
@@ -1,3 +1,4 @@
+# %%
 import pooch
 import pandas as pd
 import os
@@ -8,12 +9,12 @@ import matplotlib.pyplot as plt
 
 
 # %%
-url = "https://api.open-meteo.com/v1/meteofrance?latitude=43.6109&longitude=3.8763&daily=weathercode,temperature_2m_max,temperature_2m_min,precipitation_sum,rain_sum,snowfall_sum,precipitation_hours,precipitation_probability_max,windspeed_10m_max,winddirection_10m_dominant&timezone=Europe%2FBerlin&format=csv"
-path_target = "./weather.csv"
+url = "https://api.open-meteo.com/v1/meteofrance?latitude=43.6109&longitude=3.8763&current=rain,cloudcover&daily=temperature_2m_max,temperature_2m_min,precipitation_sum,rain_sum,snowfall_sum,windspeed_10m_max,winddirection_10m_dominant&timezone=Europe%2FBerlin&format=csv"
+path_target = "./data/weather.csv"
 path, fname = os.path.split(path_target)
 pooch.retrieve(url, path=path, fname=fname, known_hash=None)
 #%%
-df_weather_raw=pd.read_csv("weather.csv",skiprows=[0,1,2])
+df_weather_raw=pd.read_csv("weather.csv",skiprows=[0,1,2,3,4])
 df_weather_raw.head()
 # %%
 plt.plot(df_weather_raw["time"],df_weather_raw["temperature_2m_max (°C)"])
