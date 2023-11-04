c642c34d97b1270dfe1434393afe87f5e69a6bd6
Abel SILLY <abel.silly@etu.umontpellier.fr>
2023-11-04 21:34:35 +0100

    modif path_target

--- Projet/io/Load.py
+++ Projet/io/Load.py
@@ -4,18 +4,18 @@ import pandas as pd
 import os
 import matplotlib.pyplot as plt
 #%%
-#weather_path=pooch.retrieve(url="https://api.open-meteo.com/v1/meteofrance?latitude=43.6109&longitude=3.8763&daily=weathercode,temperature_2m_max,temperature_2m_min,precipitation_sum,rain_sum,snowfall_sum,precipitation_hours,precipitation_probability_max,windspeed_10m_max,winddirection_10m_dominant&timezone=Europe%2FBerlin&format=csv"
-#                            ,known_hash=None,fname="weather.csv",path="~/OneDrive/Bureau/HAX712X/Projet_Perso/data")
-
+url = "https://api.open-meteo.com/v1/meteofrance?latitude=43.6109&longitude=3.8763&current=rain,cloudcover&daily=temperature_2m_max,temperature_2m_min,precipitation_sum,rain_sum,snowfall_sum,windspeed_10m_max,winddirection_10m_dominant&timezone=Europe%2FBerlin&format=csv"
+path_target ="C:\\Users\\abels\\OneDrive\\Bureau\\HAX712X\\Projet_Perso\\Projet\\data"
+pooch.retrieve(url, path=path_target, fname=os.path.split(path_target), known_hash=None)
 
 # %%
 url = "https://api.open-meteo.com/v1/meteofrance?latitude=43.6109&longitude=3.8763&current=rain,cloudcover&daily=temperature_2m_max,temperature_2m_min,precipitation_sum,rain_sum,snowfall_sum,windspeed_10m_max,winddirection_10m_dominant&timezone=Europe%2FBerlin&format=csv"
-path_target = "./data/weather.csv"
+path_target =os.path.abspath("C:\\Users\\abels\\Bureau\\HAX712X\\Projet_Perso\\Projet\\data")
 path, fname = os.path.split(path_target)
 pooch.retrieve(url, path=path, fname=fname, known_hash=None)
 #%%
 df_weather_raw=pd.read_csv("weather.csv",skiprows=[0,1,2,3,4])#data frame daily weather
-df_weather_raw.columns=["Date","Température Max","Température Min","Précipitation","Neige","Vitesse Max du vent","Direction de vent dominante"]
+#df_weather_raw.columns=["Date","Température Max","Température Min","Précipitation","Neige","Vitesse Max du vent","Direction de vent dominante"]
 df_weather_raw.head()
 # %%
 df_cwweather_raw=pd.read_csv("weather.csv",skiprows=[0,1,2,5,6,7,8,9,10,11]) #data frame current weather
