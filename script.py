851aa26ebefd2126be53b97fe3125604947fbc65
e20200008052 <abel.silly@etu.umontpellier.fr>
2023-11-06 11:07:01 +0100

    affichage du dataframe météo

--- script.py
+++ script.py
@@ -1,2 +1,4 @@
 import Projet
-df = Projet.Load_db().save_as_df()
+df = Projet.Load_db().save_as_df()#data frame données météo
+df.columns=['Date','Température Max','Température Min','Levé du soleil','Couché du soleil','Neige','Heure de pluie','Vitesse Max du vent','Direction dominante du vent']
+print(df)
