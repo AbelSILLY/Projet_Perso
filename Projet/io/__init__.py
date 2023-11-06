852f7f7d01e2b65a9a3a8ff7f1c0678e1616bcb1
Abel SILLY <abel.silly@etu.umontpellier.fr>
2023-11-06 17:07:40 +0100

    changement url

--- Projet/io/__init__.py
+++ Projet/io/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-url_db = "https://api.open-meteo.com/v1/meteofrance?latitude=52.52&longitude=13.41&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,rain_sum,snowfall_sum,precipitation_hours,wind_speed_10m_max,wind_direction_10m_dominant&timezone=Europe%2FBerlin&format=csv"
+url_db = "https://api.open-meteo.com/v1/meteofrance?latitude=52.52&longitude=13.41&daily=weather_code,temperature_2m_max,temperature_2m_min,precipitation_sum,wind_speed_10m_max,wind_direction_10m_dominant&timezone=Europe%2FBerlin&format=csv"
 path_target = os.path.join(
     os.path.dirname(os.path.realpath(__file__)), "..", "data", "weather_db.csv"
 )
